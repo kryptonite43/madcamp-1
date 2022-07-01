dbb260ac57df334afa50d50b2390652995ee96b8
Yujin Son <66019792+kryptonite43@users.noreply.github.com>
2022-07-01 22:43:01 +0900

    Update RVAdapter.kt

--- app/src/main/java/com/example/helloworld/RVAdapter.kt
+++ app/src/main/java/com/example/helloworld/RVAdapter.kt
@@ -7,8 +7,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-//import com.bumptech.glide.Glide
-//import org.w3c.dom.Text
+import com.bumptech.glide.Glide
+import org.w3c.dom.Text
 
 class RVAdapter(val context : Context, var List : MutableList<GalleryContents>):RecyclerView.Adapter<RVAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVAdapter.ViewHolder {
