5ddb5b28f739d272742911f50fe379b54702e9d6
andyj0927 <andyj0927@kaist.ac.kr>
2022-07-02 15:48:11 +0900

    위키백과 연결

--- app/src/main/java/com/example/helloworld/ContactFragment.kt
+++ app/src/main/java/com/example/helloworld/ContactFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.helloworld.databinding.ActivityMainBinding
 import com.google.gson.Gson
 import java.io.IOException
 
--- app/src/main/java/com/example/helloworld/SplashActivity.kt
+++ app/src/main/java/com/example/helloworld/SplashActivity.kt
@@ -13,6 +13,6 @@ class SplashActivity : AppCompatActivity() {
         Handler().postDelayed({
             startActivity(Intent(this,MainActivity::class.java))
             finish()
-        }, 500)
+        }, 1000)
     }
 }
